1180565f908e0c8868cf823020f5a5fce0e90afd
ianguest <iag.1.apps@gmail.com>
2021-07-17 20:59:40 +0100

    feat: Extend AddDeck layout to include button

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View } from 'react-native'
+import { addDeck } from '../actions'
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View } from 'react-native'
 import { connect } from 'react-redux';
+import { blue, white } from '../colors'
 
 class AddDeck extends Component {
+  state = {
+    titleInput: '',
+  }
+  handleTitleChange = (titleInput) => {
+    this.setState((prevState) => ({
+      titleInput,
+    }))
+  }
+  handleCreateDeck = () => {
+    // TODO
+  }
   render() {
+    const { titleInput } = this.state;
     return (
       <View style={styles.container}>
         <Text style={styles.titleText}>What is the title of your new deck?</Text>
+        <TextInput
+          value={titleInput}
+          style={styles.titleInput}
+          onChangeText={this.handleTitleChange}
+        />
+        <TouchableOpacity onPress={this.handleCreateDeck} style={styles.button}>
+          <Text style={styles.buttonText}>
+            Create Deck
+          </Text>
+        </TouchableOpacity>
       </View>
-
     );
   }
 }
@@ -16,12 +44,32 @@ class AddDeck extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20
+    justifyContent: 'space-around',
+    padding: 20,
   },
   titleText: {
     fontSize: 25,
-    textAlign: 'center'
+    textAlign: 'center',
   },
+  titleInput: {
+    borderWidth: 1,
+    margin: 20,
+    padding: 8,
+    fontSize: 20,
+  },
+  button: {
+    backgroundColor: blue,
+    padding: 10,
+    marginLeft: 50,
+    marginRight: 50, 
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 20,
+    color: white,
+  }
 })
 
 export default connect()(AddDeck);
